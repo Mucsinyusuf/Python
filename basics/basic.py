b78a6608a0853e392dd2ec79782d9e4d677b7849
mucsinyusuf <muxsinyussuf@gmail.com>
2024-07-05 15:44:20 -0400

    Operators

---
+++ basics/basic.py
@@ -0,0 +1,66 @@
+print ("Hello World!")
+
+# Arithmetic operator
+
+# Operator	Operation	    Example
+# +	         Addition	    3 + 2 = 5
+# -        	Subtraction	    3 - 2 = 1
+# *	         Multiplication	3 * 2 = 6
+# /	          Division	    4 / 2 = 2
+# %	          Modulus (Remainder after division)	3 % 2 = 1
+
+#arethmetic shortcuts
+a= 5
+#we can add 3 to a by 
+a += 3
+
+
+print (a)
+
+
+# comparison operators
+# Operator	   Meaning	      Example
+# ==	       Equal	         1 == 2 return false
+# !=	      Not Equal	         1 != 2 return true
+# >	          Greater Than	      1 > 2 return false
+# <	        Lower Than	          1 < 2 return true
+# >=	    Greater or Equal	  1 >= 2 return false
+# <=	     Lower or Equal	     1 <= 2 return true
+
+
+# logical operators 
+
+# Logical operators are used to 
+# check combinations of comparisons 
+# that return True or False.
+
+# Operator   	Meaning	                                   Example
+# and	     And - True if all operands are True	    a and b
+# or	      Or - True if any operand is True	        a or b
+# not	      Not - True if the operand is False	    not a
+
+
+#True Table for AND operator 
+
+# a	     b	   a and b
+# False	False	False
+# False	True	False
+# True	False	False
+# True	True	True
+# you will nly get a True if both are true 
+
+# TRuth table for or Operator 
+# a	     b	    a or b
+# False	False	False
+# False	True	True
+# True	False	True
+# True	True	True
+
+# here to et true either a or b should be true 
+
+
+# Truth table for the not operator 
+# a	   not a
+# False	True
+# True	False
+#Here the value of a is reversed. If a is False then not a is True
