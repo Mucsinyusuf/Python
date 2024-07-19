2b3470c7d32c86008b75a70dd522b29abb54cbfd
mucsinyusuf <muxsinyussuf@gmail.com>
2024-07-19 07:15:18 +0300

    conditionalscont

--- int/conditionals.py
+++ int/conditionals.py
@@ -40,23 +40,65 @@
 # Bonus: Extend functionality with extra mode so it also does celsius to fahrenheit conversion
 # formula is: temp in C*9/5 + 32 = temp in f
 
-operator = input("Enter operand:\n")
-number1 = float(input("enter first number:\n"))
-if operator.lower() == 'f':
-    print((number1*9/5)+32)
-else:
-    number2 = float(input("enter second number:\n"))
+# operator = input("Enter operand:\n")
+# number1 = float(input("enter first number:\n"))
+# if operator.lower() == 'f':
+#     print((number1*9/5)+32)
+# else:
+#     number2 = float(input("enter second number:\n"))
 
-    if operator == '+':
-        print (number1 + number2)
-    elif operator == '-':
-        print(number1 - number2)
-    elif operator == '*':
-        print(number1*number2)
-    elif operator == '/':
-        print(number1/number2)
+#     if operator == '+':
+#         print (number1 + number2)
+#     elif operator == '-':
+#         print(number1 - number2)
+#     elif operator == '*':
+#         print(number1*number2)
+#     elif operator == '/':
+#         print(number1/number2)
 
-    else:
-        print("invalid oprator")
+#     else:
+#         print("invalid oprator")
 
-   
+
+# def num_days(month):
+
+#     if month == 'jan':
+#         print('number of days in',month,'is',31)
+#     elif month == 'feb':
+#         print('number of days in',month,'is',28)
+#     elif month == 'mar':
+#         print('number of days in',month,'is',31)
+#     elif month == 'apr':
+#         print('number of days in',month,'is',30)
+#     elif month == 'may':
+#         print('number of days in',month,'is',31)
+#     elif month == 'jun':
+#         print('number of days in',month,'is',30)
+#     elif month == 'jul':
+#         print('number of days in',month,'is',31)
+#     elif month == 'aug':
+#         print('number of days in',month,'is',31)
+#     elif month == 'sep':
+#         print('number of days in',month,'is',30)
+#     elif month == 'oct':
+#         print('number of days in',month,'is',31)
+#     elif month == 'nov':
+#         print('number of days in',month,'is',30)
+#     elif month == 'dec':
+#         print('number of days in',month,'is',31)
+
+# num_days('oct')
+# optimize/shorten the code in the function
+# try to reduce the number of conditionals 
+
+
+def num_days(month):
+
+    if month == 'jan' or month == 'mar' or month == 'may' or month == 'jul' or month == 'aug' or month == 'oct' or month == 'dec':
+        print('number of days in',month,'is',31)
+    elif month == 'feb':
+        print('number of days in',month,'is',28)
+    elif month == 'apr' or month == 'jun' or month == 'sep' or month == 'nov':
+        print('number of days in',month,'is',30)
+        
+num_days('oct')
